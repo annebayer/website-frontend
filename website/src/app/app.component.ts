8e154ff18c79da17f858d6977d8e603817e5accc
uvlwv <uvlwv@student.kit.edu>
2025-10-26 15:16:08 +0100

    Add Header

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router'; // <— wichtig!
-
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'Unsere Neuseeland-Reise';
+}
