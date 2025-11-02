262e5ec5191ecced7f791e1c85d0e07fd16219e6
uvlwv <uvlwv@student.kit.edu>
2025-11-02 14:20:21 +0100

    correct url for prod configuration

--- website/src/environments/environment.prod.ts
+++ website/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://deine-domain.com/api/tages?populate[pictures][populate]=*',
-  baseUrl: 'https://deine-domain.com',
-  graphqlUrl: 'https://deine-domain.com/graphql'
+  apiUrl: 'https://honest-acoustics-26b7e6e546.strapiapp.com/api/tages?populate[pictures][populate]=*',
+  baseUrl: 'https://honest-acoustics-26b7e6e546.strapiapp.com',
+  graphqlUrl: 'https://honest-acoustics-26b7e6e546.strapiapp.com/graphql'
 };
