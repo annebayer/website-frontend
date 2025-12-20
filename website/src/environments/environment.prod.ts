d27a9b7c92bcc252c50330e24eee9a1f597f354b
uvlwv <uvlwv@student.kit.edu>
2025-12-20 16:02:50 +0100

    include railway url instead of strapi

--- website/src/environments/environment.prod.ts
+++ website/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://honest-acoustics-26b7e6e546.strapiapp.com/api/tages?populate[pictures][populate]=*',
-  baseUrl: 'https://honest-acoustics-26b7e6e546.strapiapp.com',
-  graphqlUrl: 'https://honest-acoustics-26b7e6e546.strapiapp.com/graphql'
+  apiUrl: 'https://website-backend-production-d0f9.up.railway.app/api/tages?populate[pictures][populate]=*',
+  baseUrl: 'https://website-backend-production-d0f9.up.railway.app/',
+  graphqlUrl: 'https://website-backend-production-d0f9.up.railway.app//graphql'
 };
