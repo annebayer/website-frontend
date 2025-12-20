f007c391a185167705b397addac9b929f13efb0d
uvlwv <uvlwv@student.kit.edu>
2025-12-20 16:12:53 +0100

    include railway url instead of strapi

--- website/src/environments/environment.prod.ts
+++ website/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://website-backend-production-d0f9.up.railway.app/api/tages?populate[pictures][populate]=*',
-  baseUrl: 'https://website-backend-production-d0f9.up.railway.app/',
+  baseUrl: 'https://website-backend-production-d0f9.up.railway.app',
   graphqlUrl: 'https://website-backend-production-d0f9.up.railway.app/graphql'
 };
